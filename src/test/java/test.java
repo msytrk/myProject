01468015350cfff3aa0ef882f411001b3c97b0c6
Mehmet <mhmtsytrk@gmail.com>
2020-12-24 00:39:14 +0300

    Second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class test {
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("Test1");
+    }
 }
